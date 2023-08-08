0af84edd9aa39340d919cd0a62f0555e9e2906e8
Sofa <squishysparrow7.7@gmail.com>
2023-08-09 09:34:16 +1200

    Add midnight theme

--- config/lua/myconfig/plugins.lua
+++ config/lua/myconfig/plugins.lua
@@ -2,4 +2,12 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  -- Neat very dark theme
+  use {
+    'dasupradyumna/midnight.nvim',
+    -- Autoload
+    config = function()
+      vim.cmd.colorscheme "midnight"
+    end,
+  }
 end)
