569f062373f66d7363b7d063b7a6cb523a4e82a3
Sofa <squishysparrow7.7@gmail.com>
2023-08-09 09:50:36 +1200

    Add tree sitter

--- config/lua/myconfig/plugins.lua
+++ config/lua/myconfig/plugins.lua
@@ -2,6 +2,20 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  -- Tree sitter my beloved
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = ':TSUpdate',
+    config = function()
+      -- Tiny setup, so it stays here.
+      require'nvim-treesitter.configs'.setup {
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+        auto_install = true,
+        highlight = { enable = true },
+      }
+    end,
+  }
+
   -- Neat very dark theme
   use {
     'dasupradyumna/midnight.nvim',
