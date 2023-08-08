7bdab2f9a2305bcf031c439f8e826799e5b7d4db
Sofa <squishysparrow7.7@gmail.com>
2023-08-09 11:22:23 +1200

    Formatter

--- config/lua/myconfig/plugins.lua
+++ config/lua/myconfig/plugins.lua
@@ -1,27 +1,27 @@
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-  -- Tree sitter my beloved
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate',
-    config = function()
-      -- Tiny setup, so it stays here.
-      require'nvim-treesitter.configs'.setup {
-        ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
-        auto_install = true,
-        highlight = { enable = true },
-      }
-    end,
-  }
+	-- Tree sitter my beloved
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+		config = function()
+			-- Tiny setup, so it stays here.
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+				auto_install = true,
+				highlight = { enable = true },
+			})
+		end,
+	})
 
-  -- Neat very dark theme
-  use {
-    'dasupradyumna/midnight.nvim',
-    -- Autoload
-    config = function()
-      vim.cmd.colorscheme "midnight"
-    end,
-  }
+	-- Neat very dark theme
+	use({
+		"dasupradyumna/midnight.nvim",
+		-- Autoload
+		config = function()
+			vim.cmd.colorscheme("midnight")
+		end,
+	})
 end)
