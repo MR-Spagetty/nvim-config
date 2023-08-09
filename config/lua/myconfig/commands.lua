1bade39cdc9ee5ee2a50c978a59c33ec83f0a920
Sofa <squishysparrow7.7@gmail.com>
2023-08-10 02:12:39 +1200

    Create commands file
    Right now Telescope symbols and lazy git
    :Symbols and :LG

---
+++ config/lua/myconfig/commands.lua
@@ -0,0 +1,9 @@
+-- My custom commands
+-- See nvim_create_user_command({name}, {command}, {*opts})
+
+local cmd = vim.api.nvim_create_user_command
+
+cmd("Symbols", "Telescope lsp_document_symbols", {})
+
+-- My beloved
+cmd("LG", "tabnew term://lazygit | setlocal nobuflisted bufhidden=wipe", {})
--- config/lua/myconfig/init.lua
+++ config/lua/myconfig/init.lua
@@ -1,4 +1,4 @@
 require("myconfig.options")
 require("myconfig.plugins")
 require("myconfig.mappings")
-
+require("myconfig..commands")
