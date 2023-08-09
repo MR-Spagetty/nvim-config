910d5155b3685da119c0f28eb0bdd6994aea752f
Sofa <squishysparrow7.7@gmail.com>
2023-08-09 12:20:03 +1200

    Add coq for completion

--- config/lua/myconfig/config/lsp.lua
+++ config/lua/myconfig/config/lsp.lua
@@ -1,4 +1,6 @@
 local lspconfig = require("lspconfig")
+local coq = require("coq")
+local caps = coq.lsp_ensure_capabilities
 
 local servers = {
   "lua_ls",
@@ -20,13 +22,13 @@ require("neodev").setup()
 
 -- Do a default setup for all the stuff
 for _, server in ipairs(servers) do
-  lspconfig[server].setup {}
+  lspconfig[server].setup(caps({}))
 end
 
 -- {{{ lua_ls
 -- Get rid of that configure as luv message
 -- From https://github.com/LunarVim/LunarVim/issues/4049#issuecomment-1634539474
-lspconfig.lua_ls.setup {
+lspconfig.lua_ls.setup(caps({
   settings = {
     Lua = {
       workspace = {
@@ -34,5 +36,5 @@ lspconfig.lua_ls.setup {
       },
     },
   },
-}
+}))
 -- }}}
