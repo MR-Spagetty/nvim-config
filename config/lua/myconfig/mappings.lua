9b1e1f2df481346dc84317a078defc256ee1fb68
Sofa <squishysparrow7.7@gmail.com>
2023-08-10 20:48:43 +1200

    Add keymaps for git, terminal, commenting

--- config/lua/myconfig/mappings.lua
+++ config/lua/myconfig/mappings.lua
@@ -1,4 +1,4 @@
--- TODO: Use whichkey?
+local wk = require("which-key")
 vim.g.mapleader = " "
 
 -- {{{ Nice terminal splitting: start_term(vsplit | split)
@@ -6,4 +6,45 @@ vim.g.mapleader = " "
 local function start_term(split_cmd)
   vim.cmd("botright " .. split_cmd .. " term://" .. vim.env.SHELL)
 end
+-- }}}
+
+-- Normal mode
+-- {{{ Terminal starting
+wk.register({
+  v = {
+    v = { function() start_term("vsplit") end, "Vertical terminal" }
+  },
+  h = {
+    h = { function() start_term("vsplit") end, "Horizontal terminal" }
+  },
+}, { prefix = '<leader>' })
+-- }}}
+
+-- {{{ Git
+wk.register({
+  g = {
+    name = "Git",
+    -- Open fugitive to the right
+    g = { "<cmd>Git<cr><C-w>L", "Fugitive" },
+    b = { "<cmd>Gitsigns blame_line<cr>", "Show blame" },
+    s = { "<cmd>Telescope git_status<cr>", "Git status" },
+    n = { "<cmd>Gitsigns next_hunk<cr>", "Next hunk" },
+    p = { "<cmd>Gitsigns prev_hunk<cr>", "Prev hunk" },
+  },
+}, { prefix = '<leader>' })
+-- }}}
+
+-- {{{ Commenting
+wk.register({
+  ["/"] = { function() require("Comment.api").toggle.linewise.current() end, "Comment" }
+}, { mode = { "n" }, prefix = '<leader>'})
+
+wk.register({
+  ["/"] = { function()
+    -- From the Comment.nvim documentation
+    local esc = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
+    vim.api.nvim_feedkeys(esc, 'nx', false)
+    require("Comment.api").toggle.linewise(vim.fn.visualmode())
+  end, "Comment" }
+}, { mode = { "v" }, prefix = '<leader>'})
 -- }}}
