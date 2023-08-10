abb6e9d126cd8ef76720c443ffd9bd5f1dc7ca10
Sofa <squishysparrow7.7@gmail.com>
2023-08-10 22:43:57 +1200

    LSP keybinds!!

--- config/lua/myconfig/mappings.lua
+++ config/lua/myconfig/mappings.lua
@@ -59,3 +59,48 @@ wk.register({
 -- {{{ TODO: Telescope
 -- }}}
 
+-- {{{ LSP
+--  AutoCMD from lspconfig readme.md, hotkeys and such modified by me
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+  callback = function(ev)
+    local opts = { buffer = ev.buf }
+
+    wk.register({
+      e = { vim.diagnostic.open_float, "Floating diagnostic" },
+    }, { prefix = "<leader>" })
+
+    wk.register({
+      ["]d"] = { vim.diagnostic.goto_next, "Next diagnostic" },
+      ["[d"] = { vim.diagnostic.goto_prev, "Prev diagnostic" },
+      K = { vim.lsp.buf.hover, "LSP Hover", opts },
+    })
+
+    wk.register({
+      name = "LSP",
+      q = { vim.diagnostic.setloclist, "Set location list" },
+      a = { vim.lsp.buf.code_action, "Code action", opts },
+      r = { "<cmd>Telescope lsp_references<cr>", "References", opts },
+      w = {
+        name = "Workspace folders",
+        a = { vim.lsp.buf.add_workspace_folder, "Add folder", opts },
+        r = { vim.lsp.buf.remove_workspace_folder, "Remove folder", opts },
+        l = { function()
+          local str = ""
+          for i, v in ipairs(vim.lsp.buf.list_workspace_folders()) do
+            str = str .. i .. ". " .. v .. "\n"
+          end
+          print(str)
+        end, "List folders", opts },
+      },
+      s = { vim.lsp.buf.signature_help, "Signature help", opts },
+      n = { vim.lsp.buf.rename, "Rename", opts },
+      f = { function() vim.lsp.buf.format { async = true } end, "Format", opts },
+      d = { vim.lsp.buf.definition, "Definition", opts },
+      D = { vim.lsp.buf.declaration, "Declaration", opts },
+      i = { vim.lsp.buf.implementation, "Implementation", opts },
+      t = { vim.lsp.buf.type_definition, "Type definition", opts },
+    }, { prefix = '<leader>l' })
+  end,
+})
+-- }}}
