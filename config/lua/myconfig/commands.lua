be2e49db6dfed6d96b26d675469da297775e8427
Sofa <squishysparrow7.7@gmail.com>
2023-08-10 19:57:37 +1200

    Add commands for telescope symbols

--- config/lua/myconfig/commands.lua
+++ config/lua/myconfig/commands.lua
@@ -10,3 +10,11 @@ cmd("LG", "tabnew term://lazygit | setlocal nobuflisted bufhidden=wipe", {})
 
 -- Quick n Easy
 cmd("TT", "TransparentToggle", {})
+
+-- {{{ Telescope symbols
+cmd("Kaomoji",     function() require('telescope.builtin').symbols { sources = {'kaomoji'} } end, {})
+cmd("Emoji",       function() require('telescope.builtin').symbols { sources =   {'emoji'} } end, {})
+cmd("MathUnicode", function() require('telescope.builtin').symbols { sources =    {'math'} } end, {})
+cmd("MathLatex",   function() require('telescope.builtin').symbols { sources =   {'latex'} } end, {})
+-- }}}
+
