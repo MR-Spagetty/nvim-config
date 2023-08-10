6bc06f1f21af2ddffa11c872ed8fee6789625e78
Sofa <squishysparrow7.7@gmail.com>
2023-08-10 14:03:17 +1200

    Mappings file, just a helper fn

--- config/lua/myconfig/mappings.lua
+++ config/lua/myconfig/mappings.lua
@@ -1,2 +1,9 @@
 -- TODO: Use whichkey?
 vim.g.mapleader = " "
+
+-- {{{ Nice terminal splitting: start_term(vsplit | split)
+-- Settings are handled bu the autocmds
+local function start_term(split_cmd)
+  vim.cmd("botright " .. split_cmd .. " term://" .. vim.env.SHELL)
+end
+-- }}}
