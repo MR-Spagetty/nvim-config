bbd4e03c57ac867a21e69d9c6633ae2602555e3c
Sofa <squishysparrow7.7@gmail.com>
2023-08-13 17:44:39 +1200

    Toggle relative numbers when going into insert mode

--- config/lua/myconfig/autocmds.lua
+++ config/lua/myconfig/autocmds.lua
@@ -1,6 +1,20 @@
 local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
 
 -- {{{ Terminals
 -- Go instantly into insert mode, ignore line numbers
 autocmd({ "TermOpen" }, { command = "setlocal nonumber | normal a" })
+-- }}}
+
+--- {{{ Toggle relative numbers
+-- Toggle relative numbers, on for insert, off by default.
+augroup("ToggleRNu", { clear = true })
+autocmd({ "InsertEnter" }, {
+  command = "setlocal norelativenumber",
+  group = "ToggleRNu"
+})
+autocmd({ "InsertLeave" }, {
+  command = "setlocal relativenumber",
+  group = "ToggleRNu"
+})
 -- }}}
