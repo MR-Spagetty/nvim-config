30b1339bbd669854659374cc7c170d8c2d03c55c
Sofa <squishysparrow7.7@gmail.com>
2023-08-13 23:51:17 +1200

    Move requires to correct place and config project

--- config/lua/myconfig/config/telescope.lua
+++ config/lua/myconfig/config/telescope.lua
@@ -1,14 +1,20 @@
 local telescope = require("telescope")
-require('telescope').load_extension('project')
-require('telescope').load_extension('media_files')
-require('telescope').load_extension('file_browser')
--- telescope.load_extension('harpoon')
 
 telescope.setup {
   pickers = {
     colorscheme = {
       enable_preview = true
+    },
+    project = {
+      base_dirs = {
+        {'~/projects', max_depth = 4},
+      },
+      order_by = "recent"
     }
   }
 }
 
+telescope.load_extension('project')
+telescope.load_extension('media_files')
+telescope.load_extension('file_browser')
+-- telescope.load_extension('harpoon')
