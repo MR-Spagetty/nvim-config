38efee45ad782b3d4db31db1ea5b0b5d343fc382
Sofa <squishysparrow7.7@gmail.com>
2023-08-13 17:45:03 +1200

    Add makeprg for java and commented out processing lsp

--- config/lua/myconfig/autocmds.lua
+++ config/lua/myconfig/autocmds.lua
@@ -18,3 +18,35 @@ autocmd({ "InsertLeave" }, {
   group = "ToggleRNu"
 })
 -- }}}
+
+-- {{{ File specific
+-- {{{ Java makeprg gradle || java
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = "java",
+  callback = function()
+    if vim.fn.filereadable("build.gradle") == 1 then
+      vim.bo.makeprg = "gradle run"
+    else
+      vim.bo.makeprg = "java %"
+    end
+  end,
+})
+-- }}}
+--[[ TODO: Add processing extension
+-- {{{ Processing LSP
+vim.api.nvim_create_autocmd({ "FileType" }, {
+  pattern = "processing",
+  callback = function()
+    -- Start the LSP
+    vim.lsp.start {
+      name = "processing-lsp",
+      cmd = { "/usr/share/processing/processing-lsp" },
+      root_dir = vim.fs.dirname(vim.fs.find(function(name)
+        return name:match ".*%.pde$"
+      end, { type = "file" })[0]),
+    }
+
+  end,
+})
+-- }}} ]]
+-- }}}
