cbcdba85c6496f5f68344b35e78d9a9115005418
Sofa <squishysparrow7.7@gmail.com>
2023-08-19 21:05:39 +1200

    Add DAP mappings

--- config/lua/myconfig/mappings.lua
+++ config/lua/myconfig/mappings.lua
@@ -128,6 +128,37 @@ wk.register({
 }, { prefix = "<leader>h" })
 -- }}} ]]
 
+-- {{{ DAP
+-- Check :help dap-api
+wk.register({
+  name = "Debugging",
+  b = { require("dap").toggle_breakpoint, "Toggle Breakpoint" },
+  B = {
+    function()
+      require("dap").list_breakpoints(true)
+    end,
+    "List breakpoints",
+  },
+  e = { require("dap").set_exception_breakpoints, "Set exception breakpoints" },
+  D = { require("dap").clear_breakpoints, "Clear breakpoints" },
+  c = { require("dap").continue, "Continue" },
+  s = { require("dap").step_over, "Step" },
+  i = { require("dap").step_into, "Step in" },
+  o = { require("dap").step_out, "Step out" },
+  r = { require("dap").repl.toggle, "Toggle REPL" },
+  h = { require("dap").run_to_cursor, "Run to cursor" },
+  ["<Up>"] = { require("dap").up, "Stacktrace up" },
+  ["<Down>"] = { require("dap").down, "Stacktrace down" },
+  k = {
+    function()
+      require("dap").disconnect()
+      require("dap").close()
+    end,
+    "Stop debugging",
+  },
+}, { prefix = "<leader>d" })
+-- }}}
+
 -- {{{ LSP
 --  AutoCMD from lspconfig readme.md, hotkeys and such modified by me
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -169,7 +200,13 @@ vim.api.nvim_create_autocmd("LspAttach", {
       s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document symbols", opts },
       S = { vim.lsp.buf.signature_help, "Signature help", opts },
       n = { vim.lsp.buf.rename, "Rename", opts },
-      f = { function() vim.lsp.buf.format { async = true } end, "Format", opts },
+      f = {
+        function()
+          vim.lsp.buf.format { async = true }
+        end,
+        "Format",
+        opts,
+      },
       d = { vim.lsp.buf.definition, "Definition", opts },
       D = { vim.lsp.buf.declaration, "Declaration", opts },
       i = { vim.lsp.buf.implementation, "Implementation", opts },
