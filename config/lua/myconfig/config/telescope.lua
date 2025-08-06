ff56028b2b94e3073fa9c6a2e053ccf4de46110a
MR-Spagetty <54694556+MR-Spagetty@users.noreply.github.com>
2025-08-06 16:52:49 +1200

    Removed duplicate whichkey bindings for telescope stuff

--- config/lua/myconfig/config/telescope.lua
+++ config/lua/myconfig/config/telescope.lua
@@ -37,29 +37,3 @@ telescope.setup {
 telescope.load_extension "ui-select"
 telescope.load_extension "fzf"
 
-local telebuilt = require "telescope.builtin"
-local wk = require "which-key"
-wk.register({
-  name = "Telescope",
-  f = { telebuilt.find_files, "Find files" },
-  F = {
-    function()
-      telebuilt.find_files { no_ignore = true, no_ignore_parent = true, hidden = true }
-    end,
-    "Find files",
-  },
-  -- h = { "<cmd>Telescope harpoon marks<cr>", "Harpoon marks" },
-  g = { telebuilt.live_grep, "Live grep" },
-  a = { telebuilt.builtin, "Telescope builtins" },
-  t = { telebuilt.treesitter, "Treesitter symbols" },
-  q = { telebuilt.quickfix, "Quickfix" },
-  l = { telebuilt.loclist, "Location list" },
-  p = { telebuilt.oldfiles, "Previous files" },
-  b = { telebuilt.buffers, "Open buffers" },
-  j = { telebuilt.jumplist, "Jump list" },
-  h = { telebuilt.help_tags, "Help tags" },
-  m = { telebuilt.man_pages, "Man pages" },
-  o = { require("cd-project.adapter").cd_project, "Search projects" },
-  d = { vim.cmd.TodoTelescope, "Find TODOs" },
-  ["/"] = { telebuilt.current_buffer_fuzzy_find, "Fuzzy find" },
-}, { prefix = "<leader>f" })
